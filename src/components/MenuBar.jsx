ca8315e076719431c6266509862475ef7d62d719
Khatim Musabbir Ratul <kmratul971@gmail.com>
2025-06-03 00:35:44 +0600

    DaisyUI set

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 const MenuBar = (props) => {
     return (
         <div>
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>         
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-info">Info</button>
+<button className="btn btn-success">Success</button>
+<button className="btn btn-warning">Warning</button>
+<button className="btn btn-error">Error</button>       
      {props.children}
         </div>
     );
